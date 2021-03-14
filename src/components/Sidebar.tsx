e3cfe11788af65997ff6d7a1991c6733b530aa78
Sean Bailey <sean@actualsize.com>
2021-03-14 19:10:40 -0400

    feature(Home): set up grid, add headline, video, and text blocks

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import tw, { css } from "twin.macro";
 
 const Row = tw.div`font-sans font-medium text-5xl italic last-of-type:mb-12`;
-const Descrip = tw.div`font-sans font-bold text-4xl italic`;
+const Descrip = tw.div`font-sans text-4xl tracking-tight`;
 
 const Name: React.FC = ({ children }) => {
   return (
     <div
-      tw="font-sans font-black uppercase italic mt-16 mb-4 leading-none"
+      tw="font-sans font-bold uppercase mb-4 leading-none tracking-tight"
       css={css`
         font-size: 6rem;
       `}
@@ -20,17 +20,23 @@ const Name: React.FC = ({ children }) => {
 const Sidebar = (): JSX.Element => {
   return (
     <div>
-      <Name>Sean Bailey</Name>
+      <Name>
+        Sean
+        <br />
+        Bailey
+      </Name>
       <div>
         <Row>is a developer,</Row>
-        <Row>instructor,</Row>
+        <Row>teacher,</Row>
         <Row>data scientist,</Row>
         <Row>musician,</Row>
         <Row>and creative</Row>
         <Row>generalist.</Row>
       </div>
-      <Descrip>This is their personal website.</Descrip>
-      <Descrip>It is a work in progress.</Descrip>
+      <Descrip>This is their</Descrip>
+      <Descrip>personal website.</Descrip>
+      <Descrip>It is a work</Descrip>
+      <Descrip>in progress.</Descrip>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,20 +6,57 @@ import Sidebar from "../components/Sidebar";
 const Home = (): JSX.Element => {
   return (
     <div
-      tw="h-screen w-screen m-4"
+      tw="h-screen w-screen pl-4 pt-4"
       css={css`
         display: grid;
-        grid-template-columns: 4fr repeat(4, 3fr);
-        grid-template-rows: 1fr;
+        column-gap: 0.5rem;
+        grid-template-columns: fit-content(16rem) repeat(8, 2fr);
+        grid-template-rows: fit-content(9vw) 1fr fit-content(1440px);
+        grid-auto-flow: columns dense;
       `}
     >
-      <div tw="col-span-1 h-screen">
+      <div tw="col-span-1 row-start-1 row-end-4 h-screen">
         <Sidebar />
       </div>
-      <div tw="col-span-1 h-screen"></div>
-      <div tw="col-span-1 h-screen"></div>
-      <div tw="col-span-1 h-screen"></div>
-      <div tw="col-span-1 h-screen"></div>
+      <div
+        tw="uppercase tracking-tighter font-bold bg-red-500 text-white col-start-2 col-end-6 mb-2"
+        css={css`
+          font-size: 10vw;
+          line-height: 9vw;
+        `}
+      >
+        I work.
+      </div>
+      <div tw="col-start-2 col-end-4 row-start-2 row-end-4 overflow-ellipsis bg-gray-100">
+        {/* section 1 */}
+      </div>
+      <div tw="col-start-4 col-end-6 row-start-2 row-end-3 overflow-ellipsis bg-gray-200">
+        {/* section 2 */}
+      </div>
+      <div tw="col-start-4 col-end-5 row-start-3 row-end-4 overflow-ellipsis bg-gray-200">
+        {/* section 3 */}
+      </div>
+      <div tw="col-start-6 col-end-8 row-start-1 row-end-3 overflow-ellipsis bg-gray-300">
+        {/* section 4 */}
+      </div>
+      <div tw="col-start-8 col-end-10 row-start-1 row-end-3 overflow-ellipsis bg-gray-400">
+        {/* section 5 */}
+      </div>
+
+      <div tw="col-start-5 col-end-10 row-start-3 row-end-4 aspect-w-4 aspect-h-3 mt-2">
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          poster="https://image.mux.com/wGO1l75BB67qX7pgwyi8ZRiuLFgbKB02ssI00E8vggS024/thumbnail.png"
+        >
+          <source
+            src="http://stream.mux.com/wGO1l75BB67qX7pgwyi8ZRiuLFgbKB02ssI00E8vggS024/high.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
     </div>
   );
 };
